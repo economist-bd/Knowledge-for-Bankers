d8303fa24c527e374fd82fd75ae77e9ed645a4d3
MD MONGERUL HAQUE <andrewlaraujo70@gmail.com>
2025-12-24 12:17:38 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,20 @@
-const CACHE_NAME = 'banker-book-v1';
-const assets = ['index.html', 'manifest.json'];
+const CACHE_NAME = 'bankers-guide-v2';
+const assets = [
+  '/',
+  '/index.html',
+  '/content.js',
+  'https://i.ibb.co/Lhbv2L6/cover.jpg'
+];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
+  );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
 });
+
