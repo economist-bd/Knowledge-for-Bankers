9d453edc0958a386c209e01e8b88d2e7bee93f38
MD MONGERUL HAQUE <andrewlaraujo70@gmail.com>
2025-12-24 13:59:17 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,37 @@
-const CACHE_NAME = 'bankers-guide-v2';
-const assets = [
-  '/',
-  '/index.html',
-  '/content.js',
-  'https://i.ibb.co/Lhbv2L6/cover.jpg'
+const CACHE_NAME = "banking-guide-v2";
+const ASSETS = [
+    "./",
+    "./index.html",
+    "./content.js",
+    "./manifest.json",
+    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
-  );
+// Install Event
+self.addEventListener("install", (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS);
+        })
+    );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
+// Activate Event (Cleanup old caches)
+self.addEventListener("activate", (e) => {
+    e.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+            );
+        })
+    );
 });
 
+// Fetch Event (Offline Support)
+self.addEventListener("fetch", (e) => {
+    e.respondWith(
+        caches.match(e.request).then((cachedResponse) => {
+            return cachedResponse || fetch(e.request);
+        })
+    );
+});
